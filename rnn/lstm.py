2105039e47ace4355cd105d78af46af9186e9586
Justin S Bayer <bayer.justin@googlemail.com>
2010-03-30 12:55:26 +0200

    Tests for lstm transfer function and the function itself added.

--- rnn/lstm.py
+++ rnn/lstm.py
@@ -2,22 +2,20 @@
 
 import theano
 import theano.tensor as T
+sig = T.nnet.sigmoid
 
 
 lstminpt = T.vector('lstm-input')
 state = T.vector('lstm-state')
 
+lstminpt_squashed = sig(lstminpt)
 slicesize = lstminpt.shape[0] / 4
-inpt = lstminpt[:slicesize]
-ingate = lstminpt[slicesize: 2 * slicesize]
-forgetgate = lstminpt[2 * slicesize:3 * slicesize]
-outgate = lstminpt[3 * slicesize:4 * slicesize]
-
-ingate = T.nnet.sigmoid(ingate)
-forgetgate = T.nnet.sigmoid(forgetgate)
-outgate = T.nnet.sigmoid(outgate)
+inpt = lstminpt_squashed[:slicesize]
+ingate = lstminpt_squashed[slicesize: 2 * slicesize]
+forgetgate = lstminpt_squashed[2 * slicesize:3 * slicesize]
+outgate = lstminpt_squashed[3 * slicesize:4 * slicesize]
 
 new_state = inpt * ingate + state * forgetgate
-output = new_state * outgate
+output = sig(new_state) * outgate
 
 lstm = theano.function([lstminpt, state], [new_state, output])
--- rnn/tests/unittests/test_lstm.py
+++ rnn/tests/unittests/test_lstm.py
@@ -1,10 +1,25 @@
 """
 
->>> 2 + 2
-4
+>>> from rnn.lstm import lstm
+>>> inpt = scipy.array([ 0.6,  0.8,  0.2,  0.4,  0.4,  0.6,  0.8,  1. ])
+>>> initialstate = scipy.zeros(2)
+
+>>> state, outpt = lstm(inpt, initialstate)
+>>> state
+array([ 0.35500377,  0.4130792 ], dtype=float32)
+>>> outpt 
+array([ 0.405588  ,  0.43997008], dtype=float32)
+>>> state, outpt = lstm(inpt * 1.5, state)
+>>> state
+array([ 0.63761002,  0.78988129], dtype=float32)
+>>> outpt
+array([ 0.50277889,  0.56233251], dtype=float32)
 
 """
+
 __author__ = 'Justin Bayer, bayer.justin@googlemail.com'
+
+import scipy
 
 
         
